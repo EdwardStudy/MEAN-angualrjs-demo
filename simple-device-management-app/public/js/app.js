2d9ef7ed38cf48d38a77d0e67a1399effdea5c49
Zida <shawzida@gmail.com>
2015-01-23 15:33:16 +0800

    fix前端代码模块化

---
+++ simple-device-management-app/public/js/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+//Declare app level module which depends on filters, and services
+angular.module('app', ['deviceCtrl', 'devicesServices', 'ngRoute', 'ngResource'])
+    .config(['$routeProvider',
+        //setup mapping routes and templates
+        function($routeProvider){
+            $routeProvider.
+                when('/', {
+                    controller: 'deviceListCtrl',
+                    templateUrl: 'listDevices.html'
+                }).
+                when('/add', {
+                    controller: 'addDeviceCtrl',
+                    templateUrl: 'addDevice.html'
+                }).
+                when('/edit/:id', {
+                    controller: 'editDeviceCtrl',
+                    templateUrl: 'addDevice.html'
+                }).
+                otherwise({
+                    redirectTo: '/'
+                });
+    }]);
+
--- simple-device-management-app/public/js/device-service.js
+++ simple-device-management-app/public/js/device-service.js
@@ -1,40 +1,16 @@
-//create a module to support getting and managing the device list
-var deviceModule = angular.module('DeviceModule', []);
+'use strict';
 
-deviceModule.factory('Devices',['$resource', function($resource){
-    return $resource('http://localhost\\:3000/devices/:deviceId', {},
-        { update: {}, isArray: false});
+/* Services */
 
-//    var items = {};
-//
-//    items.data = [
-//            {id:0, name: "iphone", assetTag: "a23456", onwer: "dev", desc: "iOS8.1.3"},
-//            {id:1, name: "loaner-laptop-1", assetTag: "a13936", onwer: "dev", desc: ""},
-//            {id:2, name: "loaner-laptop-3", assetTag: "a43056", onwer: "qa", desc: ""},
-//            {id:3, name: "android", assetTag: "a33756", onwer: "dev", desc: "android4.2"},
-//            {id:4, name: "galaxy tab", assetTag: "a53356", onwer: "dev", desc: "android"},
-//            {id:5, name: "loaner-laptop-2", assetTag: "a65535", onwer: "qa", desc: ""},
-//            {id:6, name: "iphone", assetTag: "a73856", onwer: "dev", desc: "iOS8.1.3"},
-//        ];
-//
-//    items.query = function(){
-//        return items.data;
-//    }
-//
-//    items.add = function(device){
-//        items.data.push(device);
-//    }
-//
-//    items.update = function(device) {
-//        // find the selected device & update
-//        items.data.forEach(function (item, i) {
-//            if (item.id == device.id) {
-//                // update
-//                items.data[i] = device;
-//                return;
-//            }
-//        })
-//    }
-//
-//    return items;
-}]);
+// Demonstrate how to register services
+// In this case it is a simple value service.
+angular.module('devicesServices', [])
+    .config(['$httpProvider', function($httpProvider){
+        delete $httpProvider.defaults.headers.common["X-Requested-With"];
+    }])
+    .factory('Devices', ['$resource', function ($resource) {
+        return $resource('http://localhost\\:3000/devices/:deviceId',
+            {},
+            {update: {method:'PUT'}, isArray:false}
+        );
+    }]);
